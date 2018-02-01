af1dd4141bd634ee8ad17aea7ee61ad76e681c8b
johnware15 <contact@johndware.com>
2018-02-01 13:32:10 -0800

    Construct todo-list-item component using this and buttons

--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import React from 'react'
 import TodosListHeader from './todos-list-header'
+import TodosListItem from './todos-list-item'
 
 export default class TodosList extends React.Component {
   renderItems() {
@@ -12,9 +13,9 @@ export default class TodosList extends React.Component {
     return (
       <table>
         <TodosListHeader />
-        <tr>
+        <tbody>
           { this.renderItems() }
-        </tr>
+        </tbody>
       </table>
     );
   }
