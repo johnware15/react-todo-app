23262378c02abb83ab57b57e141c991b8db75345
johnware15 <contact@johndware.com>
2018-02-01 14:58:12 -0800

    Add styling to tasks

--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -7,10 +7,17 @@ export default class TodosList extends React.Component {
   render() {
       console.log(this.props);
     return (
-      <form>
-        <input type="text" placeholder="Add Task" />
+      <form onSubmit={this.handleCreate.bind(this)}>
+        <input type="text" placeholder="Add Task" ref="createInput" />
         <button>Add</button>
       </form>
     );
+  }
+
+  handleCreate(event) {
+    event.preventDefault()
+
+    this.props.createTask(this.refs.createInput.value)
+    this.refs.createInput.value = ''
   }
 }
